e7aa6d80e66328c41408da9869c6c2bf6472aa41
iamdan-boop <pinedadanjanus@gmail.com>
2022-10-13 14:58:51 +0800

    fixed customer name not appearing on table

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -15,13 +15,14 @@ class CustomerController extends Controller
     {
         if ($request->ajax()) {
             return DataTables::of(User::query()->role('customer'))
+                ->addColumn('name', fn (User $user) => $user->first_name . ' ' . $user->last_name)
                 ->addColumn('actions', function (User $user) {
                     return '<div>
-                        <a href="'. route('customer.show', $user) .'" class="btn btn-secondary btn-sm">View</a>
-                        <button type="button" onclick="document.getElementById('. "'deleteForm'". ').submit()" class="btn btn-dark btn-sm">Delete</button>
+                        <a href="' . route('customer.show', $user) . '" class="btn btn-secondary btn-sm">View</a>
+                        <button type="button" onclick="document.getElementById(' . "'deleteForm'" . ').submit()" class="btn btn-dark btn-sm">Delete</button>
                         <form action="' . route('customer.destroy', $user) . '" method="POST" id="deleteForm">
                                 <input type="hidden" name="_token" value="' . csrf_token() . '">
-                                '. method_field('DELETE') .'
+                                ' . method_field('DELETE') . '
                         </form>
                     </div>';
                 })
